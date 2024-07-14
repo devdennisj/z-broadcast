89cbc00dd0a34faf4e2adca3daf2801d467a0858
Dennis Johansson <dev@dennisj.se>
2024-07-14 16:53:57 +0200

    Adds second channel to show that the data doesn't get intermingled

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,24 @@ const schema = z.object({
 
 type Message = z.infer<typeof schema>;
 
+const secondSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+});
+
+type SecondMessage = z.infer<typeof secondSchema>;
+
 function App() {
   const { data, postMessage } = useChannel<Message>({
     name: "new_test_channel",
     schema,
   });
+
+  const { data: secondData, postMessage: postSecondMessage } =
+    useChannel<SecondMessage>({
+      name: "second_test_channel",
+      schema: secondSchema,
+    });
 
   return (
     <>
@@ -21,6 +34,17 @@ function App() {
         type="button"
         onClick={() => {
           postMessage({ name: "dennis" });
+        }}
+      >
+        parse
+      </button>
+
+      <h1>Second Data</h1>
+      <pre>{JSON.stringify(secondData, null, 2)}</pre>
+      <button
+        type="button"
+        onClick={() => {
+          postSecondMessage({ name: "blargh", age: 10 });
         }}
       >
         parse
