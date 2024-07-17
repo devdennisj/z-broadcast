b6908e32016efbc2b944459c2c14e3574168cc5d
Dennis Johansson <dev@dennisj.se>
2024-07-17 17:48:51 +0200

    Fixes warning in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootId = document.getElementById("root");
+
+if (!rootId) {
+  throw new Error("No root element found");
+}
+
+ReactDOM.createRoot(rootId).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
